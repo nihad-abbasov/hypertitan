de9fa71b8ef6b6d6ecf0db84a1f8611cf232c592
nihad-abbasov <nihadhadabbasov2000@gmail.com>
2023-11-14 15:37:17 +0400

    commented out className on backToTop

--- src/_components/BackToTop/BackToTop.tsx
+++ src/_components/BackToTop/BackToTop.tsx
@@ -38,7 +38,7 @@ export const BackToTop = () => {
           height="1em"
           width="1em"
           xmlns="http://www.w3.org/2000/svg"
-          className="w-full h-full"
+          // className="w-full h-full"
         >
           <path d="M12.0001 4.83594L5.79297 11.043L7.20718 12.4573L12.0001 7.66436L16.793 12.4573L18.2072 11.043L12.0001 4.83594ZM12.0001 10.4858L5.79297 16.6929L7.20718 18.1072L12.0001 13.3143L16.793 18.1072L18.2072 16.6929L12.0001 10.4858Z"></path>
         </svg>
